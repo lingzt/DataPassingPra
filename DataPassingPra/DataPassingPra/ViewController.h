a4f50cd8299f63bad47761b0862ea314b465087c
Ling <asklingtoby@gmail.com>
2016-06-21 10:02:51 -0400

    set up test project

---
+++ DataPassingPra/DataPassingPra/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataPassingPra
+//
+//  Created by ling toby on 6/21/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
